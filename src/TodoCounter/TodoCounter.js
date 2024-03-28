6316f7ba1785280796c2d8741baaa71a30c6c822
SBellido <sebastianbellidodg@gmail.com>
2024-03-28 14:22:17 -0300

    Icon´s states are dinamics

--- src/TodoCounter/TodoCounter.js
+++ src/TodoCounter/TodoCounter.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
+  const message = total === 0
+    ? 'La bandeja de TODOS está vacía'
+    : total === completed
+    ? '¡Felicidades! ¡Has completado todos tus TODOS!'
+    : `Has completado ${completed} de ${total} TODOS`;
+
   return (
     <h1 className="TodoCounter">
-      Has completado {completed} de {total} TODOS
+      {message}
     </h1>
   );
 }
