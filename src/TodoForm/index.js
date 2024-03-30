4415c027274c29e7f50ace301ace8f509926ac85
SBellido <sebastianbellidodg@gmail.com>
2024-03-30 13:24:16 -0300

    Ready to deploy

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -25,7 +25,7 @@ function TodoForm() {
 
   return (
     <form onSubmit={onSubmit}>
-      <label>Escribe tu nuevo TODO</label>
+      <label>Escribe un nuevo TODO</label>
       <textarea 
         placeholder="Ej: Programar reunión"
         value={newTodoValue}
