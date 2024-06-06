6a7ac7e9275900a012e4a2af1e0be3452c8e2eab
SBellido <sebastianbellidodg@gmail.com>
2024-06-06 15:48:17 -0300

    Commented code was deleted

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,30 +1,3 @@
-// import './TodoList.css';
-
-// function TodoList(props) {
-//   const renderFunc = props.children || props.render;
-
-//   return (
-//     <section className='TodoList-container'>
-//       {props.error && props.onError()}
-//       {props.loading && props.onLoading()}
-
-//       {(!props.loading && !props.totalTodos) && props.onEmptyTodos()}
-
-//       {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResults(props.searchText)}
-
-//       {/* {props.searchedTodos.map(props.render)} */}
-
-//       {props.searchedTodos.map(renderFunc)}
-
-//       <ul className="TodoList">
-//         {props.children}
-//       </ul>
-//     </section>
-//   );
-// }
-
-// export { TodoList };
-
 import "./TodoList.css";
 
 function TodoList(props) {
