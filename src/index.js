e0ea31454915785c0ad1aa49449630d1cd3b920e
SBellido <sebastianbellidodg@gmail.com>
2024-06-07 17:37:47 -0300

    Trying with High Order Components in App Component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+// import App from "./App/index.js";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 /**
  * Index
@@ -13,14 +12,31 @@ import reportWebVitals from "./reportWebVitals";
  * También inicia la medición del rendimiento de la aplicación si se desea.
  */
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App(props) {
+  return (
+    <h1>
+      ¡{props.saludo}, {props.nombre}!
+    </h1>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function withSaludo(saludo) {
+  return function WrappedComponentWithSaludo(WrappedComponent) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      );
+    };
+  };
+}
+
+const AppWithSaludo = withSaludo("Hola")(App);
+
+ReactDOM.render(
+  <AppWithSaludo nombre="Juanita" />,
+  // <App saludo="Buenas" nombre="Nath" />,
+  document.getElementById("root")
+);
